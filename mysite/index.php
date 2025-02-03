3511b7d004badb0bde04a7f9546dddee0497ab3d
MLSMason6 <mschrader1@una.edu>
2025-02-03 17:55:04 -0600

    Add files via upload

---
+++ mysite/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        CIS 476
+        <?php
+        // put your code here
+
+        $var = 3;
+        echo "hi<p>";
+
+        echo sqrt($var);
+        ?>
+
+        <form action="welcome.php" method="POST">
+            Name: <input type="text" name="name"><br>
+            E-mail: <input type="text" name="email"><br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
---
+++ mysite/welcome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        // var_dump($_POST);
+        echo"<hr><p>";
+        echo "Welcome " . $_POST["name"];
+        ?>!
+        Your email address is: <?php echo $_POST["email"]; ?>
+    </body>
+</html>
